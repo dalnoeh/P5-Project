6bc5dba643777cb348689d73a3f3141ef6bbcade
dalnoeh <kbejer15@student.aau.dk>
2017-10-18 10:35:39 +0200

    Initial Project
    Basic gun mechanics

---
+++ P5 project/Assets/Gun.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+   
+
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+	}
+
+    void Shoot()
+    {
+       // muzzleFlash.Play();
+
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+           // GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+           // Destroy(impactGO, 1f);
+        }
+
+
+
+    }
+}
